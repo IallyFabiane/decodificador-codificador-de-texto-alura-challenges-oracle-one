1cb2c27c612028e55cf6680287678fe5e487cfdf
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-12-19 21:54:18 -0300

    ajustes no layout e  função visibilityNone

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 let input =  document.querySelector(".texto");
 let texto = input.value;
 
+function visibilityNone () {
+        document.getElementById("container-mensagem-erro").style.display = "none";
+   
+};
+
 console.log(texto);

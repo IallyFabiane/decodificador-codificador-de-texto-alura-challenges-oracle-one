9eccab92fe1f5cb3c99d51a21c013e96071b7375
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-12-20 12:56:13 -0300

    implementando funções mostraMensagemDeErro e exibirCriptografia

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-let input =  document.querySelector(".texto");
-let texto = input.value;
+let frase =  document.querySelector(".texto");
 
-function visibilityNone () {
-        document.getElementById("container-mensagem-erro").style.display = "none";
-   
+function criptografar (frase) {
+    let texto = frase.value.split("");
+    //console.log(texto);
+
+    texto.forEach((element, i) => {
+        if (element == "a") {
+            texto[i] = "ai";
+         } else if ( element == "e") {
+           texto[i] = "enter";
+         } else if (element == "i") {
+            texto[i] =  "imes";
+         } else if (element == "o") {
+            texto[i] = "ober";
+         } else if (element == "u") {
+            texto[i] =  "ufat";
+        }
+
+    });
+    
+    //console.log(texto.join(""));
+    return texto.join("");
+}
+
+function exibirCriptografia() {
+   let frase =  document.querySelector(".texto");
+   let texto = document.querySelector(".container-texto");
+   let containerRetangulo = document.querySelector(".container-retangulo");
+   texto.innerHTML = criptografar(frase);
+   mostraMensagemDeErro(true);
+   containerRetangulo.innerHTML = criptografar(frase);
+}
+
+function mostraMensagemDeErro (bool) {
+    document.getElementById("container-mensagem-erro").style.display = "none";
 };
 
-console.log(texto);
